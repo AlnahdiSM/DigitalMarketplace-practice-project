7f26bf890ae164f1a8673b7c6c7e8f42b3107c94
Salem Alnahdi <alnahdi4b@gmail.com>
2024-07-24 18:19:06 +0300

    chore: Update MaxWidthWrapper component to use wider screen size

--- components/MaxWidthWrapper.tsx
+++ components/MaxWidthWrapper.tsx
@@ -7,5 +7,5 @@ export default function MaxWidthWrapper({
   className?: string;
   children?: React.ReactNode;
 }) {
-  return <div className={cn("mx-auto w-full max-w-xl px-2.5 md:px-20", className)}>{children}</div>;
+  return <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>{children}</div>;
 }
